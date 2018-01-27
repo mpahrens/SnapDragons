913c31986a87952718aeeaf3539e17b0cd238925
Matt Ahrens <matthew.ahrens@tufts.edu>
2018-01-27 16:15:28 -0500

    technical demo1: transparent glSurfaceView

--- android/src/run/sid/sd/AndroidLauncher.java
+++ android/src/run/sid/sd/AndroidLauncher.java
@@ -1,33 +1,40 @@
 package run.sid.sd;
 
 import android.graphics.PixelFormat;
-import android.hardware.camera2.CameraDevice;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import android.view.SurfaceView;
 
-import android.hardware.camera2.CameraManager;
-
 import run.sid.sd.SnapDragons;
 
 public class AndroidLauncher extends AndroidApplication {
+    private View view;
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.r = 8;
-		config.g = 8;
-		config.b = 8;
-		config.a = 8;
-		if(graphics.getView() instanceof SurfaceView){
-			SurfaceView glView = (SurfaceView) graphics.getView();
-		  	glView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-		}
-        CameraManager cameraManager = getApplicationContext().getSystemService(CameraManager.class);
-		initialize(new SnapDragons(), config);
-	}
+        config.r = config.g = config.b = config.a = 8;
+        view = initializeForView(new SnapDragons(), config);
+
+
+        //initialize(new SnapDragons(), config);
+        if (graphics.getView() instanceof SurfaceView) {
+            GLSurfaceView glView = (GLSurfaceView) graphics.getView();
+            glView.setZOrderOnTop(true);
+            glView.setBackgroundColor(android.graphics.Color.TRANSPARENT);
+            glView.getHolder().setFormat(PixelFormat.RGBA_8888);
+        }
+
+
+        this.addContentView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
 
 	public void post(Runnable r){
 		handler.post(r);
--- core/src/run/sid/sd/SnapDragons.java
+++ core/src/run/sid/sd/SnapDragons.java
@@ -2,32 +2,100 @@ package run.sid.sd;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class SnapDragons extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	Batch batch;
+	Stage stage;
+	Camera camera;
+	Viewport viewport;
+	ShapeRenderer shapeRenderer;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		viewport = new StretchViewport(1920, 1080, camera);
+		stage = new Stage(viewport);
+		batch = stage.getBatch();
+		shapeRenderer = new ShapeRenderer();
+
+		Gdx.graphics.setContinuousRendering(false);
+		Gdx.graphics.requestRendering();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void resize(int width, int height) {
+		stage.getViewport().setWorldSize(1920, 1080);
+		stage.getViewport().update(width, height, true);
+		Gdx.graphics.requestRendering();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		Gdx.gl20.glClearColor(0, 1, 0, 1);
+		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		// draw whatever we have on the stage
+		stage.draw();
+
+		// draw red rectangle
+		drawRect(10, 10, 1000, 1000);
+
+		// draw a blue box
+		fillRect(50, 50, 800, 800);
+
+		// punch a hole in the surface (ie, clear an area) so that we can see the view that is
+		// behind this view
+		clearRect(200, 200, 1600, 600);
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	public void drawRect(final int x, final int y, final int width, final int height) {
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+		shapeRenderer.rect(x, y, width, height, Color.RED, Color.RED, Color.RED, Color.RED);
+		shapeRenderer.end();
+	}
+
+	public void fillRect(final int x, final int y, final int width, final int height) {
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.rect(x, y, width, height, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE);
+		shapeRenderer.end();
+	}
+
+	public void clearRect(final int x, final int y, final int width, final int height) {
+//                Gdx.gl.glEnable(GL20.GL_BLEND);
+//                Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.CLEAR);
+		shapeRenderer.rect(x, y, width, height);
+		shapeRenderer.end();
+
+//                Gdx.gl.glDisable(GL20.GL_BLEND);
 	}
 }
